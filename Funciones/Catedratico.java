187a0f6d6e95d5b673d0378b561cfadbec25f689
EBMH <brownherrera11@gmail.com>
2023-04-24 13:10:58 -0600

    Modificaciones Clase Catedratico

--- Funciones/Catedratico.java
+++ Funciones/Catedratico.java
@@ -1,49 +1,60 @@
 public class Catedratico {
-    private String nombre;
-    private int RFC;
-
-    public DocenteDatos(String nombre, int RFC) {
-        this.nombre = nombre;
-        this.RFC = RFC;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getRFC() {
-        return RFC;
-    }
-
-    public void setRFC(int RFC) {
-        this.RFC = RFC;
-    }
 
     private String nombre;
     private int RFC;
 
     public DocenteDatos(String nombre, int RFC) {
+
         this.nombre = nombre;
         this.RFC = RFC;
     }
 
     public String getNombre() {
+
         return nombre;
     }
 
     public void setNombre(String nombre) {
+
         this.nombre = nombre;
     }
 
     public int getRFC() {
+
         return RFC;
     }
 
     public void setRFC(int RFC) {
+
+        this.RFC = RFC;
+    }
+
+    private String nombre;
+    private int RFC;
+
+    public DocenteDatos(String nombre, int RFC) {
+
+        this.nombre = nombre;
+        this.RFC = RFC;
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+    }
+
+    public int getRFC() {
+
+        return RFC;
+    }
+
+    public void setRFC(int RFC) {
+
         this.RFC = RFC;
     }
 }
