8012d76784996ac654a4a8aa0460f9820cababa3
EBMH <brownherrera11@gmail.com>
2023-04-25 12:57:05 -0600

    Cree clase Catedratico

---
+++ Funciones/Catedratico.java
@@ -0,0 +1,66 @@
+public class Catedratico {
+
+    private String nombre;
+    private int RFC;
+
+    public DocenteDatos(String nombre, int RFC) {
+
+
+        private class Catedratico {
+            private String nombre;
+            private int RFC;
+
+            public Catedratico(String nombre, int RFC) {
+
+                this.nombre = nombre;
+                this.RFC = RFC;
+            }
+
+            public String getNombre() {
+
+                return nombre;
+            }
+
+            public void setNombre(String nombre) {
+
+                this.nombre = nombre;
+            }
+
+            public int getRFC() {
+
+                return RFC;
+            }
+
+            public void setRFC(int RFC) {
+
+                this.RFC = RFC;
+            }
+
+            private String nombre;
+            private int RFC;
+
+
+        this.nombre = nombre;
+        this.RFC = RFC;
+        }
+
+        public String getNombre() {
+
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+
+            this.nombre = nombre;
+        }
+
+        public int getRFC() {
+
+            return RFC;
+        }
+
+        public void setRFC(int RFC) {
+
+            this.RFC = RFC;
+        }
+    }
